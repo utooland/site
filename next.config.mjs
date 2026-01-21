036bb927677fa3941b272a3142406a49d27dbce6
elrrrrrrr <elrrrrrrr@gmail.com>
2026-01-21 13:52:12 +0800

    feat: quick install

--- next.config.mjs
+++ next.config.mjs
@@ -10,5 +10,35 @@ export default withNextra({
   i18n: {
     locales: ['en', 'zh'],
     defaultLocale: 'en'
+  },
+  async headers() {
+    return [
+      {
+        source: '/install',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain; charset=utf-8'
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600'
+          }
+        ]
+      },
+      {
+        source: '/install.ps1',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain; charset=utf-8'
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600'
+          }
+        ]
+      }
+    ]
   }
 })
