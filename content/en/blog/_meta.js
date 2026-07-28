40011b384ecf2a1af782ccf5768e723ff7b8bff9
zoomdong <1344492820@qq.com>
2026-07-28 16:53:56 +0800

    docs: publish Utoopack qiankun adaptation article

--- content/en/blog/_meta.js
+++ content/en/blog/_meta.js
@@ -10,5 +10,6 @@ export default {
     }
   },
   index: 'All posts',
+  'utoopack-qiankun': 'When Turbopack Meets qiankun: Adapting Utoopack for Micro Frontends',
   'utoopack-intro': '@utoo/pack: A Next-Generation Build Tool Based on Turbopack'
 }
--- content/zh/blog/_meta.js
+++ content/zh/blog/_meta.js
@@ -10,5 +10,6 @@ export default {
     }
   },
   index: '全部文章',
+  'utoopack-qiankun': '当 Turbopack 遇上 qiankun：Utoopack 的微前端适配实践',
   'utoopack-intro': '@utoo/pack：基于 Turbopack 的下一代构建工具'
 }
